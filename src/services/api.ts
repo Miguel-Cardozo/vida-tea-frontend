4ea7d40b2693422833e162ff25e1d0c83665f0e3
MIGUEL CARDOZO GOMES <miguelcardozogomes@gmail.com>
2026-06-14 21:01:22 -0300

    Integra login com backend

---
+++ src/services/api.ts
@@ -0,0 +1,18 @@
+const API_URL = "http://localhost:3000";
+
+export async function apiFetch(endpoint: string, options: RequestInit = {}) {
+  const response = await fetch(`${API_URL}${endpoint}`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    ...options,
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Erro na requisição");
+  }
+
+  return data;
+}
